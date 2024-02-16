d6f387b0576849ef74c63dd64200d9a9d22de1ea
doniyor <doniyorjumaniyazov5@gmail.com>
2024-02-16 15:08:16 +0500

    add first example

---
+++ lesson/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Второе первое число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num1 == num2*num2)
+{
+    Console.Write.Line("Да");
+}
+else
+{
+    Console.Write.Line("Нет");
+}
+
