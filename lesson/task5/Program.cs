0f7377e1a36b7a1ac6beba6e3b67f582c2aea851
doniyor <doniyorjumaniyazov5@gmail.com>
2024-02-16 18:44:58 +0500

    нашли сумму первай и последних цифр трехзначного число

---
+++ lesson/task5/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num%10;
+int num2 = num/100;
+
+Console.WriteLine($"Сумма равно {num1+num2} ");
