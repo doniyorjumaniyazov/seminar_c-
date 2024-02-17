a2b5be87c97786d1f82507f49534515071c6e8d8
doniyor <doniyorjumaniyazov5@gmail.com>
2024-02-17 16:39:23 +0500

    мы свободном образе приготовили задач и попиталыс решать

---
+++ lesson/task10/Program.cs
@@ -0,0 +1,26 @@
+﻿// Создайте программу, которая запрашивает у пользователя его имя и затем
+// выводит на экран приветствие с использованием этого имени
+
+// Console.Write("Whats your name?: ");
+// string name = Console.ReadLine() ?? "";
+// String greeting = "You are welcome, " + name + "!";;
+
+// Console.WriteLine(greeting);
+
+// Напишите программу, которая находит сумму двух чисел, введенных пользователем
+
+Console.Write("Первое число");
+string input1 = Console.ReadLine() ?? "";
+
+
+Console.Write("Второе число");
+string input2 = Console.ReadLine() ?? "";
+
+int num1 = Convert.ToInt32(input1);
+int num2 = Convert.ToInt32(input2);
+
+int sum = (num1 + num2);
+
+
+Console.WriteLine($"Сумма число {num1} и {num2} равно {sum}");
+
