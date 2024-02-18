dcb8c3bbcab4b5007334a74071795f8e514ed55b
doniyor <doniyorjumaniyazov5@gmail.com>
2024-02-18 13:00:43 +0500

    Делали примеры с элементарним математических элементамы как калькулятор

--- lesson/task10/Program.cs
+++ lesson/task10/Program.cs
@@ -9,18 +9,39 @@
 
 // Напишите программу, которая находит сумму двух чисел, введенных пользователем
 
-Console.Write("Первое число");
-string input1 = Console.ReadLine() ?? "";
+// Console.Write("Первое число");
+// string input1 = Console.ReadLine() ?? "";
 
 
-Console.Write("Второе число");
-string input2 = Console.ReadLine() ?? "";
+// Console.Write("Второе число");
+// string input2 = Console.ReadLine() ?? "";
 
-int num1 = Convert.ToInt32(input1);
-int num2 = Convert.ToInt32(input2);
+// int num1 = Convert.ToInt32(input1);
+// int num2 = Convert.ToInt32(input2);
 
-int sum = (num1 + num2);
+// int sum = (num1 + num2);
 
 
-Console.WriteLine($"Сумма число {num1} и {num2} равно {sum}");
+// Console.WriteLine($"Равно {sum}");
 
+// Калькулятор: Создайте простой калькулятор, который может
+//  выполнять основные арифметические операции (+, -, *, /) с двумя числами
+
+Console.Write("первое число: ");
+string num1 = Console.ReadLine();
+
+Console.Write("Втрое число: ");
+string num2 = Console.ReadLine();
+
+int number1 = Convert.ToInt32(num1);
+int number2 = Convert.ToInt32(num2);
+
+int sum = number1 + number2;
+int sum1 = number1 - number2;
+int sum2 = number1*number2;
+int sum3 = number1/number2;
+
+Console.WriteLine($"{number1} + {number2} равно {sum}");
+Console.WriteLine($"{number1} - {number2} равно {sum1}");
+Console.WriteLine($"{number1} * {number2} равно {sum2}");
+Console.WriteLine($"{number1} / {number2} равно {sum3}");
