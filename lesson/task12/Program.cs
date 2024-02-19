87aebc6a9e3acdacc4bab55fe7ae6007bf5241a9
doniyor <doniyorjumaniyazov5@gmail.com>
2024-02-19 17:26:59 +0500

    well done second work

--- lesson/task12/Program.cs
+++ lesson/task12/Program.cs
@@ -1,12 +1,26 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и удаляет вторую
 // цифру этого числа.
 
+// Console.Write("вводите трехзначние число: ");
+// string number = (Console.ReadLine())! ;
+
+// int number1 = Convert.ToInt32(number);
+
+   //     int num = number1 / 10;
+
+     //   Console.WriteLine($"резултать: {num}");
+
+// Напишите программу, которая принимает на вход трёхзначное число и возводит вторую цифру этого
+// числа в степень, равную третьей цифре. 
+
 Console.Write("вводите трехзначние число: ");
-string number = (Console.ReadLine())! ;
+string input = Console.ReadLine()! ;
 
-int number1 = Convert.ToInt32(number);
+int number = Convert.ToInt32(input);
 
-        int num = number1 / 10;
+int number1 = (number/10) %10;
+int number2 = number %10;
 
-        Console.WriteLine($"резултать: {num}");
-        
+double result = Math.Pow(number1, number2);
+
+Console.WriteLine($"Релутат:{result}");
