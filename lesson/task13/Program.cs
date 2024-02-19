017261486931eb6136376b9700136e8174ca4d9e
doniyor <doniyorjumaniyazov5@gmail.com>
2024-02-19 18:02:58 +0500

    точно сработоло

---
+++ lesson/task13/Program.cs
@@ -0,0 +1,28 @@
+﻿
+    {
+        Console.Write("Введите первое число: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        if (number2 == 0)
+        {
+            Console.WriteLine("Деление на ноль невозможно!");
+            return;
+        }
+
+        if (number1 % number2 == 0)
+        {
+            Console.WriteLine($"{number1} кратно {number2}");
+        }
+        else
+        {
+            Console.WriteLine($"{number1} не кратно {number2}. Остаток: {number1 % number2}");
+        }
+    }
+
+
+
+       
+     
