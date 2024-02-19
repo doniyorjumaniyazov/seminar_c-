f6cd230eb72beb91748fadb0b43f5ff1ed77dfc2
doniyor <doniyorjumaniyazov5@gmail.com>
2024-02-19 19:57:24 +0500

    well done homework

---
+++ lesson/task14/Program.cs
@@ -0,0 +1,20 @@
+﻿ // Напишите программу, которая принимает на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23
+
+Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number1 = 7;
+int number2 = 23;
+int number3 = number1 * number2;
+if (number % number3 == 0)
+
+{
+    Console.WriteLine($"введенный число кратно одновременно {number1}, {number2}");
+
+}
+else
+{
+    Console.WriteLine($"введенный число не кратно {number1}, {number2}");
+
+}
