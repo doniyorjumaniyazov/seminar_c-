be61440deb489fccf6d3d617ef3a34bb29ca50fd
doniyor <doniyorjumaniyazov5@gmail.com>
2024-02-19 16:25:07 +0500

    doneс

---
+++ lesson/task12/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+
+Console.Write("вводите трехзначние число: ");
+int number = int.Parse(Console.ReadLine());
+
+int number1 = Convert.ToInt32(number);
+
+int num = number1/10;
+
+Console.WriteLine($"резултать {num}");
