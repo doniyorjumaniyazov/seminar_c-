58bc0280dd8345813f4cc8edb5f83887de3e17c0
doniyor <doniyorjumaniyazov5@gmail.com>
2024-02-21 18:15:55 +0500

    well done HW

--- lesson/task16/Program.cs
+++ lesson/task16/Program.cs
@@ -3,87 +3,123 @@
 
 //Console.Write("Введите число: ");
 
-  //      int number = Convert.ToInt32(Console.ReadLine());
+//      int number = Convert.ToInt32(Console.ReadLine());
 
-    //     if (number % 7 == 0 && number % 23 == 0)
-      //   {
-        //     Console.WriteLine($"{number} (кратно одновременно) и 7, и 23.");
-         // }
-         // else
-         // {
-            // Console.WriteLine($"{number} (не кратно одновременно) 7 и 23.");
-         //}
+//     if (number % 7 == 0 && number % 23 == 0)
+//   {
+//     Console.WriteLine($"{number} (кратно одновременно) и 7, и 23.");
+// }
+// else
+// {
+// Console.WriteLine($"{number} (не кратно одновременно) 7 и 23.");
+//}
 
-         // Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0
-         // и выдаёт номер координатной четверти плоскости,в которой находится эта точка.
+// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0
+// и выдаёт номер координатной четверти плоскости,в которой находится эта точка.
 
-         //Console.Write("введите x: ");
-         //double x = Convert.ToDouble(Console.ReadLine());
-         //Console.Write("введите y: ");
-         //double y = Convert.ToDouble(Console.ReadLine());
-         
-       //if (x > 0 && y > 0)
-        //{
-          //  Console.WriteLine("Точка находится в первой координатной четверти");
-        //}
-        //else if (x < 0 && y > 0)
-        //{
-          //  Console.WriteLine("Точка находится во второй координатной четверти");
-        //}
-       // else if (x < 0 && y < 0)
-        //{
-          //  Console.WriteLine("Точка находится в третьей координатной четверти");
-        //}
-        //else if (x > 0 && y < 0)
-        //{
-          //  Console.WriteLine("Точка находится в четвёртой координатной четверти");
-        //}
+//Console.Write("введите x: ");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.Write("введите y: ");
+//double y = Convert.ToDouble(Console.ReadLine());
 
-        
-        // 3) //Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру.
+//if (x > 0 && y > 0)
+//{
+//  Console.WriteLine("Точка находится в первой координатной четверти");
+//}
+//else if (x < 0 && y > 0)
+//{
+//  Console.WriteLine("Точка находится во второй координатной четверти");
+//}
+// else if (x < 0 && y < 0)
+//{
+//  Console.WriteLine("Точка находится в третьей координатной четверти");
+//}
+//else if (x > 0 && y < 0)
+//{
+//  Console.WriteLine("Точка находится в четвёртой координатной четверти");
+//}
 
 
-         // Console.Write("Введите число: ");
-          //int number = Convert.ToInt32(Console.ReadLine());
-         
-          //if (number >= 10 && number <= 99 )
-         //{
-          //int first = number / 10;
-          //int second = number % 10;
-         
-          //int max = Math.Max(first, second);
-          //Console.WriteLine($"Наибольшая цифра число: {number} --- {max}");
-         //}
+// 3) //Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру.
 
- // Напишите программу, которая на вход принимает натуральное число N, а на
- // выходе показывает его цифры через запятую.
 
-       Console.Write("Введите натуральное число N: ");
-        int N = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
 
-        if (N < 1)
+//if (number >= 10 && number <= 99 )
+//{
+//int first = number / 10;
+//int second = number % 10;
+
+//int max = Math.Max(first, second);
+//Console.WriteLine($"Наибольшая цифра число: {number} --- {max}");
+//}
+
+// Напишите программу, которая на вход принимает натуральное число N, а на
+// выходе показывает его цифры через запятую.
+
+//  Console.Write("Введите натуральное число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// if (N < 1)
+// {
+//   Console.WriteLine("Введено некорректное число.");
+//   return;
+//  }
+
+// int i = 1;
+
+//  while (i <= N)
+//{
+// if (i == N)
+// {
+////   Console.Write($"{i}" );
+//}//
+//// else
+// {
+// Console.Write($"{i}, ");
+// }
+
+// i++;
+// }
+
+
+
+
+    
+ 
+    
+        Console.Write("Введите натуральное число n:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Цифры числа через запятую:");
+        ShowDigits(n);
+    
+
+    static void ShowDigits(int n)
+    {
+        if (n < 0)
         {
-            Console.WriteLine("Введено некорректное число.");
+            Console.WriteLine("Введите соответствующее число.");
             return;
         }
 
         int i = 1;
-
-        while (i <= N)
+        while (n / i >= 10)
         {
-            if (i == N)
-            {
-                Console.Write($"{i}" );
-            }
-            else
-            {
-                Console.Write($"{i}, ");
-            }
-            
-            i++;
+            i = i*10;
         }
- 
 
+        while (i > 0)
+        {
+            int digit = n / i;
+            Console.Write(digit);
+            n %= i;
+            i /= 10;
+            if (n > 0)
+            {
+                Console.Write(", ");
+            }
+        }
+    }
 
-
-         
