ca94a0c18dd73ed0165f64dde55e624d7c701856
doniyor <doniyorjumaniyazov5@gmail.com>
2024-02-22 21:53:49 +0500

    welldone

--- lesson/task17/Program.cs
+++ lesson/task17/Program.cs
@@ -100,37 +100,67 @@
 // т.д. Результат запишите в новый массив.
 
 
-        int[] arr = new int[9];
-        Random rand = new Random();
+        // int[] arr = new int[9];
+        // Random rand = new Random();
 
         
-        for (int i = 0; i < arr.Length; i++)
-        {
-            arr[i] = rand.Next(-10, 15); 
-        }       
+        // for (int i = 0; i < arr.Length; i++)
+        // {
+           // arr[i] = rand.Next(-10, 15); 
+        // }       
 
-        Console.WriteLine("Исходный массив:");
+        //Console.WriteLine("Исходный массив:");
         
-        foreach (int number in arr)
+        // foreach (int number in arr)
+        // {
+           // Console.Write(number + ", ");
+        // }
+        // Console.WriteLine(); 
+
+        // Console.WriteLine("Произведения пар чисел в массиве:");
+        // for (int i = 0; i < arr.Length / 2; i++)
+        // {
+           // int Product = arr[i] * arr[arr.Length - 1 - i];
+            //Console.WriteLine(Product);
+        //}
+ 
+ //       if (arr.Length % 2 != 0)
+   //     {
+     //       int nonProduct = arr.Length / 2;
+       //     Console.WriteLine($"Элемент {arr[nonProduct]} не имеет пары.");
+        // }
+
+       // Дано натуральное трёхзначное число. Создайте массив, состоящий из
+// цифр этого числа. Младший разряд числа должен располагаться на 0-
+// м индексе массива, старший – на 2-м.
+
+            Console.Write("Введите трехзначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number >= 100 && number <= 999) // Проверяем, что введено трехзначное число
         {
-            Console.Write(number + ", ");
-        }
-        Console.WriteLine(); 
+            int[] digits = new int[3]; // Создаем массив для цифр числа
 
-        Console.WriteLine("Произведения пар чисел в массиве:");
-        for (int i = 0; i < arr.Length / 2; i++)
+            // Разбиваем число на цифры и заполняем массив
+            digits[0] = number % 10; // Младший разряд числа
+            digits[1] = (number / 10) % 10; // Средний разряд числа
+            digits[2] = number / 100; // Старший разряд числа
+
+            // Выводим массив на экран
+            Console.Write("[");
+            for (int i = 0; i < digits.Length; i++)
+            {
+                Console.Write(digits[i]);
+                if (i != digits.Length - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine("]");
+        }
+        else
         {
-            int Product = arr[i] * arr[arr.Length - 1 - i];
-            Console.WriteLine(Product);
+            Console.WriteLine("Ошибка: введено неправильное число. Пожалуйста, введите трехзначное число.");
         }
-
-        if (arr.Length % 2 != 0)
-        {
-            int nonProduct = arr.Length / 2;
-            Console.WriteLine($"Элемент {arr[nonProduct]} не имеет пары.");
-        }
-    
-
-
     
 
